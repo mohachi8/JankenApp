454dafb75b093556dbd8f7fd44b13b7762f2a5d4
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-10-24 16:36:35 +0900

    All done random JankenApp.text ~P166

--- app/src/main/java/com/example/jankenapp/MainActivity.kt
+++ app/src/main/java/com/example/jankenapp/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.jankenapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.TextKeyListener.clear
 import android.view.View
+import androidx.core.content.edit
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 import com.example.jankenapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         binding.gu.setOnClickListener { onJankenButtonTapped(it) }
         binding.choki.setOnClickListener { onJankenButtonTapped(it) }
         binding.pa.setOnClickListener { onJankenButtonTapped(it) }
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit{
+            clear()
+        }
     }
 
     fun onJankenButtonTapped(view: View?){
